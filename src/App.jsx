5fd2828edf0835126f46e28b3443c7aeae2b6ef5
coffmanjrp <coffmanjrp@gmail.com>
2021-02-06 01:27:09 -0800

    update useFetchJobs hook

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ import Job from './Job';
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
-  const { jobs, loading, error } = useFetchJobs();
+  const { jobs, loading, error, hasNextPage } = useFetchJobs();
 
   return (
     <Container className="my-4">
       <h1 className="mb-4">React GitHub Jobs App</h1>
-      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
       {jobs.map((job) => {
         return <Job key={job.id} job={job} />;
       })}
-      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   );
 }
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,6 +5,7 @@ const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
   ERROR: 'error',
+  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page',
 };
 
 const BASE_URL =
@@ -23,6 +24,8 @@ function reducer(state, action) {
         jobs: [],
         error: action.payload.error,
       };
+    case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+      return { ...state, hasNextPage: action.payload.hasNextPage };
     default:
       return state;
   }
@@ -32,11 +35,11 @@ const useFetchJobs = (params, page) => {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
   useEffect(() => {
-    const cancelToken = axios.CancelToken.source();
+    const cancelToken1 = axios.CancelToken.source();
     dispatch({ type: ACTIONS.MAKE_REQUEST });
     axios
       .get(BASE_URL, {
-        cancelToken: cancelToken.token,
+        cancelToken: cancelToken1.token,
         params: { markdown: true, page, ...params },
       })
       .then((res) => {
@@ -50,8 +53,29 @@ const useFetchJobs = (params, page) => {
         dispatch({ type: ACTIONS.ERROR, payload: { error } });
       });
 
+    const cancelToken2 = axios.CancelToken.source();
+    axios
+      .get(BASE_URL, {
+        cancelToken: cancelToken2.token,
+        params: { markdown: true, page: page + 1, ...params },
+      })
+      .then((res) => {
+        dispatch({
+          type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
+          payload: { hasNextPage: res.data.length !== 0 },
+        });
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) {
+          return;
+        }
+
+        dispatch({ type: ACTIONS.ERROR, payload: { error } });
+      });
+
     return () => {
-      cancelToken.cancel();
+      cancelToken1.cancel();
+      cancelToken2.cancel();
     };
   }, [params, page]);
 
